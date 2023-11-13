c560857037a04ad81d58a295c610505f6e697ffc
Alexandre Nicolas <117630727+elbalexandre@users.noreply.github.com>
2023-11-13 16:28:25 +0100

    chore(template): use edge runtime in API endpoints (#18)
    # What this PR does?
    
    - configure edge runtime on every template API endpoints

--- template/src/app/api/users/cron/start-sync-jobs/route.ts
+++ template/src/app/api/users/cron/start-sync-jobs/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 
+export const runtime = 'edge';
+
 export function GET() {
   return new NextResponse(null, { status: 501, statusText: 'Not Implemented' });
 }
