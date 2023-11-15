ec7b4dcf7a7008240e1338d617a3625b985ea450
Alexandre Nicolas <117630727+elbalexandre@users.noreply.github.com>
2023-11-15 14:13:59 +0100

    refactor(template): update crons (#27)
    ## Description
    According to the specs of the cron lifecycle:
    - remove `start-*` crons
    - rename `pick-*` by `run-*` (in singular instead of plural)
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [X] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.

--- template/src/app/api/authentication/cron/run-sync-job/service.ts
+++ template/src/app/api/authentication/cron/run-sync-job/service.ts
@@ -1,2 +1,2 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-function -- this is a placeholder
-export const pickDataProtectionSyncJobs = () => {};
+export const runAuthenticationSyncJob = () => {};
