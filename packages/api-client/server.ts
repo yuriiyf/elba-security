5c88768cb6521c59b57a66ba3ac83ad517dc6907
Alexandre Nicolas <117630727+elbalexandre@users.noreply.github.com>
2024-02-09 09:54:44 +0100

    feat(api-client): add verbose mode (#117)
    ## Description
    
    - add `-v` argument to `start` script that enable verbose mode
    - when verbose mode is on every request bodies are logged
    
    ## Additional Notes
    
    N/A
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [X] I have performed a self-review of my own code.
    - [X] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.

--- packages/api-client/server.ts
+++ packages/api-client/server.ts
@@ -1,4 +1,5 @@
 import { serve } from '@hono/node-server';
+import type { Context, Next } from 'hono';
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import { elbaApiRoutes } from './src/routes';
@@ -8,7 +9,21 @@ const app = new Hono();
 const port = 3522;
 const pathPrefix = '/api/rest';
 
+const isVerboseModeEnabled = process.argv[2] && process.argv[2] === '-v';
+
+const logRequestBody = async (context: Context, next: Next) => {
+  const requestBody: unknown = await context.req.raw.clone().json();
+  // eslint-disable-next-line no-console -- To display request json body
+  console.log(JSON.stringify(requestBody, null, 2));
+  await next();
+};
+
 app.use('*', logger());
+
+if (isVerboseModeEnabled) {
+  app.use('*', logRequestBody);
+}
+
 for (const route of elbaApiRoutes) {
   // TODO: handle authentication
   app[route.method](`${pathPrefix}${route.path}`, async ({ req: { raw: request } }) =>
