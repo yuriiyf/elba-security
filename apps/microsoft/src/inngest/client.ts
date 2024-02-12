339fb87d6b28f72cbcecbc03e422113ffe369490
Alexandre Nicolas <117630727+elbalexandre@users.noreply.github.com>
2024-02-12 18:17:18 +0100

    fix(microsoft): missing inngest unauthorized middleware (#124)
    ## Description
    
    - add missing inngest unauthorized middleware
    
    ## Additional Notes
    
    n/a
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [X] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.

--- apps/microsoft/src/inngest/client.ts
+++ apps/microsoft/src/inngest/client.ts
@@ -2,6 +2,7 @@ import { EventSchemas, Inngest } from 'inngest';
 import { sentryMiddleware } from '@elba-security/inngest';
 import { logger } from '@elba-security/logger';
 import { rateLimitMiddleware } from './middlewares/rate-limit-middleware';
+import { unauthorizedMiddleware } from './middlewares/unauthorized-middleware';
 
 export const inngest = new Inngest({
   id: 'microsoft',
@@ -52,6 +53,6 @@ export const inngest = new Inngest({
       };
     };
   }>(),
-  middleware: [rateLimitMiddleware, sentryMiddleware],
+  middleware: [rateLimitMiddleware, unauthorizedMiddleware, sentryMiddleware],
   logger,
 });
