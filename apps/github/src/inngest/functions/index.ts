b04b0d9526329face7285ee146fae1559272e5ff
Alexandre Nicolas <117630727+elbalexandre@users.noreply.github.com>
2024-02-12 10:19:42 +0100

    fix(github): add inngest function removeOrganisation export (#118)
    ## Description
    
    - add missing `removeOrganisation` inngest function export
    
    ## Additional Notes
    
    N/A
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [X] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.

--- apps/github/src/inngest/functions/index.ts
+++ apps/github/src/inngest/functions/index.ts
@@ -1,9 +1,11 @@
+import { removeOrganisation } from './organisations/remove-organisation';
 import { scheduleAppsSyncs } from './third-party-apps/schedule-apps-syncs';
 import { syncAppsPage } from './third-party-apps/sync-apps-page';
 import { scheduleUsersSyncs } from './users/schedule-users-syncs';
 import { syncUsersPage } from './users/sync-users-page';
 
 export const inngestFunctions = [
+  removeOrganisation,
   syncUsersPage,
   scheduleUsersSyncs,
   syncAppsPage,
