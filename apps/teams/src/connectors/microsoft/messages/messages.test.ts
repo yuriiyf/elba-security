f5b5f622e567c45adb76124fd6d1393d7b5bd710
Yurii <159924530+yuriiyf@users.noreply.github.com>
2024-03-12 11:12:11 +0200

    feat(teams): initial data-protection sync (#154)
    ## Description
    
    Implementation of data-protection sync for Teams with agreed mapping to
    elba. The sync follows the same pattern from slack, where firstly we
    need to get the whole list of teams with pagination, then list of
    channels per each team (pagination not supported for this request), then
    each messages per channel and after all each replies per messages. All
    this done with `waitForEvent`. **Additionally I have added filter to
    user sync, to get only Member of organisation and skip the guests, as it
    was agreed.**
    
    ## Additional Notes
    
    There could be upcoming changes to add tests for inngest functions and
    small code improvements. Also I will rename events to `.requested`
    format as it done for microsoft integration.
    
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [x] I have performed a self-review of my own code.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [x] I have added tests to cover the new feature or fixes.

---
+++ apps/teams/src/connectors/microsoft/messages/messages.test.ts
@@ -0,0 +1,147 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call -- test conveniency */
+/* eslint-disable @typescript-eslint/no-unsafe-return -- test conveniency */
+
+import { http } from 'msw';
+import { describe, expect, test, beforeEach } from 'vitest';
+import { env } from '@/env';
+import type { MicrosoftMessage } from '@/connectors/microsoft/types';
+import { getMessages } from '@/connectors/microsoft/messages/messages';
+import { server } from '../../../../vitest/setup-msw-handlers';
+import { MicrosoftError } from '../commons/error';
+
+const teamId = 'some-team-id';
+const channelId = 'some-channel-id';
+
+const validToken = 'token-1234';
+const startSkipToken = 'start-skip-token';
+const endSkipToken = 'end-skip-token';
+const nextSkipToken = 'next-skip-token';
+
+const invalidMessages = [
+  {
+    id: `some-id-1`,
+    webUrl: `http://wb.uk.com`,
+    etag: `293891203`,
+    createdDateTime: `2023-03-28T21:11:12.395Z`,
+    lastEditedDateTime: `2024-02-28T21:11:12.395Z`,
+    messageType: 'typing',
+  },
+  {
+    id: `some-id-2`,
+    webUrl: `http://wb.uk.com`,
+    etag: `293891203`,
+    createdDateTime: `2023-03-28T21:11:12.395Z`,
+    lastEditedDateTime: `2024-02-28T21:11:12.395Z`,
+    messageType: 'chatEvent',
+  },
+];
+
+function createValidMessagesArray() {
+  const objectsArray: MicrosoftMessage[] = [];
+
+  for (let i = 0; i < Number(env.MESSAGES_SYNC_BATCH_SIZE) - invalidMessages.length; i++) {
+    const obj: MicrosoftMessage = {
+      id: `some-id-${i}`,
+      webUrl: `http://wb.uk-${i}.com`,
+      etag: `122123213`,
+      createdDateTime: `2023-03-28T21:11:12.395Z`,
+      lastEditedDateTime: `2024-02-28T21:11:12.395Z`,
+      body: {
+        content: 'test',
+      },
+      from: {
+        user: {
+          id: `user-id-${i}`,
+        },
+      },
+      messageType: 'message',
+      type: 'message',
+    };
+    objectsArray.push(obj);
+  }
+
+  return objectsArray;
+}
+
+const validMessages = createValidMessagesArray();
+
+const messages = [...validMessages, ...invalidMessages];
+describe('getMessages', () => {
+  beforeEach(() => {
+    server.use(
+      http.get(
+        `${env.MICROSOFT_API_URL}/teams/:teamId/channels/:channelId/messages`,
+        ({ request, params }) => {
+          if (
+            request.headers.get('Authorization') !== `Bearer ${validToken}` ||
+            params.teamId !== teamId ||
+            params.channelId !== channelId
+          ) {
+            return new Response(undefined, { status: 401 });
+          }
+
+          const url = new URL(request.url);
+          const top = url.searchParams.get('$top');
+          const skipToken = url.searchParams.get('$skiptoken');
+
+          const nextPageUrl = new URL(url);
+          nextPageUrl.searchParams.set('$skiptoken', nextSkipToken);
+
+          return Response.json({
+            '@odata.nextLink':
+              skipToken === endSkipToken ? null : decodeURIComponent(nextPageUrl.toString()),
+            value: messages.slice(0, top ? Number(top) : 0),
+          });
+        }
+      )
+    );
+  });
+
+  test('should return messages and nextSkipToken when the token, teamId and channelId is valid and their is another page', async () => {
+    await expect(
+      getMessages({ teamId, channelId, token: validToken, skipToken: startSkipToken })
+    ).resolves.toStrictEqual({
+      nextSkipToken,
+      invalidMessages,
+      validMessages,
+    });
+  });
+
+  test('should return messages and no nextSkipToken when the token,teamId and channelId is valid and their is no other page', async () => {
+    await expect(
+      getMessages({ teamId, channelId, token: validToken, skipToken: endSkipToken })
+    ).resolves.toStrictEqual({
+      invalidMessages,
+      validMessages,
+      nextSkipToken: null,
+    });
+  });
+
+  test('should throws when the token is invalid', async () => {
+    await expect(
+      getMessages({ teamId, channelId, token: 'invalid-token', skipToken: startSkipToken })
+    ).rejects.toBeInstanceOf(MicrosoftError);
+  });
+
+  test('should throws when the teamId is invalid and their is another page', async () => {
+    await expect(
+      getMessages({
+        channelId,
+        teamId: 'invalid-tenant-id',
+        token: validToken,
+        skipToken: startSkipToken,
+      })
+    ).rejects.toBeInstanceOf(MicrosoftError);
+  });
+
+  test('should throws when the channelId is invalid and their is another page', async () => {
+    await expect(
+      getMessages({
+        channelId: 'invalid-channel-id',
+        teamId,
+        token: validToken,
+        skipToken: startSkipToken,
+      })
+    ).rejects.toBeInstanceOf(MicrosoftError);
+  });
+});
---
+++ apps/teams/src/connectors/microsoft/schemes.ts
@@ -0,0 +1,25 @@
+import { z } from 'zod';
+
+export const messageSchema = z.object({
+  id: z.string(),
+  webUrl: z.string().url(),
+  body: z.object({
+    content: z.string(),
+  }),
+  from: z.object({
+    user: z.object({
+      id: z.string(),
+    }),
+  }),
+  lastEditedDateTime: z.string().nullable(),
+  createdDateTime: z.string(),
+  etag: z.string(),
+  messageType: z.enum([
+    'typing',
+    'message',
+    'chatEvent',
+    'unknownFutureValue',
+    'systemEventMessage',
+  ]),
+  type: z.enum(['message', 'reply']),
+});
