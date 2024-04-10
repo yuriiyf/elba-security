ae85eea821a6d481464bfea85d4733c3975b0dbb
Yurii <yurii.finohenov@gmail.com>
2024-04-10 19:01:38 +0300

    chore: remove drizzle migration files

--- apps/teams/drizzle/0001_bouncy_silhouette.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "channels" RENAME COLUMN "membershipType" TO "membership_type";--> statement-breakpoint
-ALTER TABLE "channels" RENAME COLUMN "displayName" TO "display_name";--> statement-breakpoint
-ALTER TABLE "channels" ADD COLUMN "channel_id" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "channels" DROP COLUMN IF EXISTS "messages";
