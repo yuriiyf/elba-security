1aa29258c8d4a5d571d854b19a160d12446325db
Guillaume PROQUIN <proquig@users.noreply.github.com>
2024-06-11 15:32:41 +0200

    fix(google): temporarily disable unauthorized middleware (#323)
    ## Description
    
    This temporarily disables inngest unauthorized to prevent false
    positives
    
    ## Additional Notes
    
    Add any other context or screenshots about the feature request here.
    
    ## Checklist:
    
    Before you create this PR, confirm all the requirements listed below by
    checking the checkboxes like this (`[x]`).
    
    - [ ] I have performed a self-review of my own code.
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have added tests to cover the new feature or fixes.

--- apps/google/src/inngest/client.ts
+++ apps/google/src/inngest/client.ts
@@ -2,11 +2,11 @@ import { EventSchemas, Inngest } from 'inngest';
 import { sentryMiddleware } from '@elba-security/inngest';
 import { logger } from '@elba-security/logger';
 import type { InngestEvents } from './functions';
-import { googleUnauthorizedMiddleware } from './middlewares/google-unauthorized-middleware';
+// import { googleUnauthorizedMiddleware } from './middlewares/google-unauthorized-middleware';
 
 export const inngest = new Inngest({
   id: 'google',
   schemas: new EventSchemas().fromRecord<InngestEvents>(),
-  middleware: [googleUnauthorizedMiddleware, sentryMiddleware],
+  middleware: [/*googleUnauthorizedMiddleware, */ sentryMiddleware],
   logger,
 });
